13e1b6bc30d76562f9e8c06313fd1a85053b57f9
Andrii Kicha <acousticdesk@gmail.com>
2022-11-16 18:57:00 +0200

    Update Assignment_1.cpp

--- MPI/Assignment_1.cpp
+++ MPI/Assignment_1.cpp
@@ -1,23 +1,23 @@
-//#include "mpi.h"
-//#include <stdio.h>
-//#include <string>
-//bool isEven(int num)
-//{
-//	return num % 2 == 0;
-//}
-//
-//int main(int argc, char* argv[])
-//{
-//	int size, rank, i;
-//	MPI_Init(&argc, &argv);
-//	MPI_Comm_size(MPI_COMM_WORLD, &size);
-//	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-//	if (rank == 0)
-//	{
-//		printf("Amount of processes=%d\n", size);
-//	}
-//	std::string evenOrOdd = isEven(rank) ? "even" : "odd";
-//	printf("My rank is %d. I am %s", rank, evenOrOdd.c_str());
-//	MPI_Finalize();
-//	return 0;
-//}
+#include "mpi.h"
+#include <stdio.h>
+#include <string>
+bool isEven(int num)
+{
+	return num % 2 == 0;
+}
+
+int main(int argc, char* argv[])
+{
+	int size, rank, i;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	if (rank == 0)
+	{
+		printf("Amount of processes=%d\n", size);
+	}
+	std::string evenOrOdd = isEven(rank) ? "even" : "odd";
+	printf("My rank is %d. I am %s", rank, evenOrOdd.c_str());
+	MPI_Finalize();
+	return 0;
+}
